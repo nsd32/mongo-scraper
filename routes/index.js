0d7c24d3753e78fef26768af620dc6d850434f8a
Lea Abeyta <lea.abeyta@gmail.com>
2017-11-19 10:56:49 -0700

    started UI and checking articles against db

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Article = require('../models/article');
-
-router.get('/', function(req, res, next) {
-	return res.send('Hello World');
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -5,22 +5,34 @@ const cheerio = require('cheerio');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-const routes = require('./routes/index');
 const Article = require('./models/article');
 
 const app = express();
 
+// Database connection and remove
 mongoose.Promise = Promise;
-
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongoHeadlines';
 mongoose.connect(MONGODB_URI);
-
-app.use('/', routes);
-
 var db = mongoose.connection;
+// Article.remove({}, function(err, article) {
+// 	if (err) {
+// 		console.log(err);
+// 	}
+// })
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Telling express to get static files from the public folder
+app.use(express.static('public'));
 
 
-
+// HTTP request scraping html
 request("http://www.nbcsports.com/nba", (error, response, html) => {
 	var $ = cheerio.load(html);
 
@@ -44,19 +56,46 @@ request("http://www.nbcsports.com/nba", (error, response, html) => {
 			photo: photo
 		}
 
-		console.log('Record inserted!');
-		console.log(JSON.stringify(article, null, 2));
+		// console.log('Record inserted!');
+		// console.log(JSON.stringify(article, null, 2));
 
-		Article.create(article, function(error, article) {
-			if (error) {
-				console.log(error)
+		Article.findOne({ headline: headline }, function(err, headline) {
+			if (err) {
+				console.log(err);
+				return
 			}
-		})
+
+			if (!headline) {
+				Article.create(article, function(error, article) {
+					if (error) {
+						console.log(error);
+						return;
+					}
+					console.log('Article added!')
+				});
+			} else {
+				console.log('Article already exists!');
+			}
+		});
+
+		
 	})
 });
 
 
+// Routes
+app.get('/', function(req, res) {
+	Article.find({}, function(err, articles) {
+		res.render('index', { articles: articles });
+	});
+});
 
+app.get('/all', function(req, res) {
+	// db.articledata.find(function(err, articles) {
+	// 	res.json(articles);
+	// })
+	
+});
 
 
 
